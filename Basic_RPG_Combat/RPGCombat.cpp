b5452b3082b55ab250415791fc353c93b011d20f
Matthew Sanders <sandemat@oregonstate.edu>
2022-05-03 16:29:29 -0600

    some final modularization and exception handling; added choose attack and item functions, a set health function to character to add more health to enemy characters

--- Basic_RPG_Combat/RPGCombat.cpp
+++ Basic_RPG_Combat/RPGCombat.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <stdexcept>
+#include <exception>
 
 
 class Attack {
@@ -122,7 +124,7 @@ public:
 	void doAttack(std::string attackName, Character& other) {
 
 		std::cout << m_name << " attacks " << other.getName();
-		std::cout << " using " << attackName << std::endl;
+		std::cout << " using " << attackName << "\n";
 		
 		if (strengthMultiplier > 0) {
 			other.reactToAttack(findAttack(attackName) * strengthMultiplier);
@@ -135,46 +137,50 @@ public:
 		}
 
 		if (other.getHealth() <= 0) {
-			std::cout << std::endl << m_name << " has killed " << other.getName() << "!\n\n";
+			std::cout << "\n" << m_name << " has killed " << other.getName() << "!\n\n";
 		}
 
 	}
 
 	virtual void useItem(std::string itemName) {
 
-		std::cout << m_name << " uses " << itemName << std::endl;
+		std::cout << m_name << " uses " << itemName << "\n";
 
 		health += findItem(itemName);
 	}
 
 	// Prints player stats to the console
 	virtual void printStats() {
-		std::cout << m_name << std::endl;
-		std::cout << "\tName: " << m_name << std::endl;
-		std::cout << "\tCurrent Health: " << health << std::endl;
+		std::cout << m_name << "\n";
+		std::cout << "\tName: " << m_name << "\n";
+		std::cout << "\tCurrent Health: " << health << "\n";
 		std::cout << "\tAttacks: ";
 
 		for (auto attack : attackSet) {
 			std::cout << attack.getName() << ", ";
 		}
-		std::cout << std::endl;
+		std::cout << "\n";
 
 		std::cout << "\tItems: ";
 
 		for (auto item : itemSet) {
 			std::cout << item.getName() << ", ";
 		}
-		std::cout << std::endl;
+		std::cout << "\n";
 
-		std::cout << "\tStrength Multiplier: " << strengthMultiplier << std::endl;
-		std::cout << "\tDefense Multiplier: " << defenseMultiplier << std::endl;
-		std::cout << std::endl;
+		std::cout << "\tStrength Multiplier: " << strengthMultiplier << "\n";
+		std::cout << "\tDefense Multiplier: " << defenseMultiplier << "\n";
+		std::cout << "\n";
 	}
 
 protected:
 
 	std::vector<Attack> attackSet;  // Set of attacks for the character
 	std::vector<Item> itemSet;  // Set of items for the character
+
+	void setHealth(double value) {
+		health = value;
+	}
 
 	// Updates a Characters health based on attack
 	void reactToAttack(double attackStrength) {
@@ -211,10 +217,13 @@ public:
 	// Initializer inherited from Character class
 	using Character::Character;
 
+	// Protected function inherited from Character class
+	using Character::setHealth;
+
 	// Overridden display function for enemy characters
 	void printStats() override {
 		std::cout << this->getName();
-		std::cout << " - Current Health: " << this->getHealth() << std::endl;
+		std::cout << " - Current Health: " << this->getHealth() << "\n";
 	}
 
 private:
@@ -228,112 +237,191 @@ private:
 
 };
 
+std::string chooseAttack(Character& player) {
+	
+	std::string userChoice;
+	int choice = -1;
+
+	std::cout << "You've chosen to attack!\n\nAttacks:\n\n";
+
+	do {
+		int i = 1;
+		// Display attack options
+		for (auto attack : player.getAttackList()) {
+			std::cout << i << ". " << attack.getName() << "\n";
+			i++;
+		}
+		std::cout << "\n";
+
+		std::cout << "Choose which attack you'd like to use (1 or 2): ";
+		std::getline(std::cin, userChoice);
+
+		/*
+		 * Attempts to convert the user choice to an integer
+		 * and find the chosen attack in the players attack list
+		*/ 
+		try {
+			choice = std::stoi(userChoice);
+			userChoice = player.getAttackList()[--choice].getName();
+		}
+		catch (std::out_of_range& e) {
+			std::cout << e.what();
+			std::cout << "Not a valid choice! try again.\n";
+			continue;
+		}
+		catch (std::invalid_argument& e) {
+			std::cout << e.what();
+			std::cout << "You did not enter a valid number! try again.\n";
+			continue;
+		}
+		catch (std::exception& e) {
+			std::cout << e.what();
+			std::cout << "You did not enter a valid number! try again.\n\n";
+			continue;
+		}
+
+	} while (choice == -1);
+
+	// Returns name of chosen attack
+	return userChoice;
+}
+
+std::string chooseItem(Character& player) {
+
+	std::string userChoice;
+	int choice = -1;
+	
+	std::cout << "You've chosen to use an Item!\n\nItems:\n\n";
+
+	do {
+		int i = 1;
+		// Display item options
+		for (auto item : player.getItemList()) {
+			std::cout << i << ". " << item.getName() << "\n";
+			i++;
+		}
+		std::cout << "\n";
+
+		std::cout << "Choose which item you'd like to use (1 or 2): ";
+		std::getline(std::cin, userChoice);
+
+		/*
+		 * Attempts to convert the user choice to an integer
+		 * and find the chosen item in the player's list of items
+		*/ 
+		try {
+			choice = std::stoi(userChoice);
+			userChoice = player.getItemList()[--choice].getName();
+		}
+		catch (std::out_of_range& e) {
+			std::cout << e.what();
+			std::cout << "Not a valid choice! try again.\n";
+			continue;
+		}
+		catch (std::invalid_argument& e) {
+			std::cout << e.what();
+			std::cout << "You did not enter a valid number! try again.\n";
+			continue;
+		}
+		catch (std::exception& e) {
+			std::cout << e.what();
+			std::cout << "You did not enter a valid number! try again.\n\n";
+			continue;
+		}
+
+	} while (choice == -1);
+
+	// Return the chosen item's name
+	return userChoice;
+}
+
 
 void playGame(Character& player, std::queue<Enemy>& enemies) {
 
 	std::string userChoice;
+	int choice;
 	
 	// Prints starting player information
 	std::cout << "Player:\n\n";
 	player.printStats();
-	std::cout << std::endl;
+	std::cout << "\n";
 	
 	std::cout << player.getName() << " Finds himself trapped in a dungeon!\n";
 	std::cout << "There's a series of rooms in front of him, he enters a room...\n\n";
 	
 	// Start game loop
 	while (!enemies.empty() && player.getHealth() > 0.0) {
-		std::cout << "A monster appears! It looks like a " << enemies.front().getName() << std::endl;
+		std::cout << "A monster appears! It looks like a " << enemies.front().getName() << "\n";
 
 		// Loop while the current enemy still has health
 		while (enemies.front().getHealth() > 0.0 && player.getHealth() > 0.0) {
 
 			// Print the enemies stats and the players current health
 			enemies.front().printStats();
-			std::cout << "\nyour current health: " << player.getHealth() << std::endl;
+			std::cout << "\nyour current health: " << player.getHealth() << "\n";
 
 			std::cout << "\nEnter 1 to attack or 2 to use an item and prepare for attack:\n";
 			std::getline(std::cin, userChoice);
 
-
-			// Options for player choosing to attack
-			if (userChoice.compare("1") == 0) {
-
-				std::cout << "You've chosen to attack!\n\nAttacks:\n\n";
-				int i = 1;
-
-				// Display attack options
-				for (auto attack : player.getAttackList()) {
-					std::cout << i << ". " << attack.getName() << std::endl;
-					i++;
-				}
-				std::cout << std::endl;
-
-				std::cout << "Choose which attack you'd like to use (1 or 2): ";
-				std::getline(std::cin, userChoice);
-
-				// Attack the monster with the player's chosen attack
-				if (userChoice.compare("1") == 0) {
-					player.doAttack("Sword To The Face", enemies.front());
-				}
-				else if (userChoice.compare("2") == 0) {
-					player.doAttack("Stab To The Heart", enemies.front());
-				}
-				else {
-					std::cout << "Not a valid choice, try again\n\n";
-					continue;
-				}
+			// Attempts to convert the user choice to an integer
+			try {
+				choice = std::stoi(userChoice);
 			}
-			// Options for player choosing to use an item
-			else if (userChoice.compare("2") == 0) {
-				
-				std::cout << "You've chosen to use an Item!\n\nItems:\n\n";
-				int i = 1;
-
-				// Display item options
-				for (auto item : player.getItemList()) {
-					std::cout << i << ". " << item.getName() << std::endl;
-					i++;
-				}
-				std::cout << std::endl;
-
-				std::cout << "Choose which item you'd like to use (1 or 2): ";
-				std::getline(std::cin, userChoice);
-
-				// Use the player's chosen item to increase health
-				if (userChoice.compare("1") == 0) {
-					player.useItem("Grog");
-				}
-				else if (userChoice.compare("2") == 0) {
-					player.useItem("Roasted Meat");
-				}
-				else {
-					std::cout << "Not a valid choice, try again\n\n";
-					continue;
-				}
-
+			catch(std::out_of_range& e){
+				std::cout << e.what();
+				std::cout << "Not a valid choice! try again.\n";
+				continue;
 			}
-			else {
-				std::cout << "Not a valid choice, try again\n\n";
+			catch (std::invalid_argument& e) {
+				std::cout << e.what();
+				std::cout << "You did not enter a valid number! try again.\n";
+				continue;
+			}
+			catch (std::exception& e) {
+				std::cout << e.what();
+				std::cout << "You did not enter a valid number! try again.\n\n";
 				continue;
 			}
 
-			// Enemy retaliates with an attack if it's not been killed 
-			if (enemies.front().getHealth() > 0) {
-				
-				std::cout << enemies.front().getName() << " retaliates with an attack!\n";
-				int randAttack = rand() % 2;  // Generate a 0 or 1 to pick an attack at random
+			switch (choice) {
+			
+			// Attack the enemy
+			case 1:
+				player.doAttack(chooseAttack(player), enemies.front());
 
-				// Enemy attacks player
-				std::cout << std::endl;
-				enemies.front().doAttack(enemies.front().getAttackList()[randAttack].getName(), player);
+				// Enemy retaliates with an attack if it's not been killed 
+				if (enemies.front().getHealth() > 0) {
+
+					std::cout << enemies.front().getName() << " retaliates with an attack!\n";
+
+					// Generate a 0 or 1 to pick an attack at random
+					int randAttack = rand() % 2;
+
+					// Enemy attacks player
+					std::cout << "\n";
+					enemies.front().doAttack(enemies.front().getAttackList()[randAttack].getName(), player);
+				}
+				break;
+
+			// Use a healing item
+			case 2:
+				player.useItem(chooseItem(player));
+				break;
+
+			default:
+				std::cout << "Not a valid choice! try again.\n";
+				continue;
 			}
+
+			
 		}
 
-		if (player.getHealth() > 0.0) {
+		// Remove enemy from queue
+		enemies.pop();
+
+		if (player.getHealth() > 0.0 && !enemies.empty()) {
 			// Let the player enter the next room after defeating the current monster
 			std::cout << "\nHe enters the next room...";
-			enemies.pop();
 		}
 	}
 	
@@ -365,6 +453,10 @@ int main() {
 	Enemy dragon("Dragon", 5, 12, dragonAttacks, enemyItems);
 	Enemy hydra("Hydra", 5, 10, hydraAttacks, enemyItems);
 	Enemy slime("Slime", 0, 5, slimeAttacks, enemyItems);
+
+	// Give each enemy different levels of health
+	dragon.setHealth(150.0);
+	hydra.setHealth(125.5);
 
 	// Queue of enemies
 	std::queue<Enemy> enemyList;
